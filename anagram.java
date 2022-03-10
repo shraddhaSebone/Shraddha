49b63735aced48fde7d2d9fbd4271c0ff39d009b
shraddhaSebone <101252859+shraddhaSebone@users.noreply.github.com>
2022-03-10 23:17:55 +0530

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -1,28 +1,26 @@
 package hello;
 import java.util.*;
 public class anagram {
-   public static void main(String[] args) {
-	   Scanner sc=new Scanner(System.in);
-	    System.out.println("First String ");
-	    String str1=sc.nextLine();
-	   // str1.replaceAll("//s","");
-	    System.out.println("Second String ");
-	    String str2=sc.nextLine();
-	    char a[] = str1.toLowerCase().toCharArray();
-	    char b[] = str2.toLowerCase().toCharArray();
-	    
-	    Arrays.sort(a);
-	    Arrays.sort(b);
-	    
-	    boolean result=Arrays.equals(a,b);
-	     
-	    if(result==true)
-	    {
-	    	System.out.println("Strings are anagram");
-	    }
-	    else
-	    {
-	    	System.out.println("Strings are not anagram");
-	    }
-   }
-}
+  public static void main(String[]args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter string1 ");
+		String str1= sc.nextLine();
+		System.out.println("enter string2");
+		String str2 = sc.nextLine();
+		anagram r = new anagram();
+		r.ana(str1, str2);
+	}
+		public void ana(String str1,String str2) {
+			
+	        char a[]=str1.toLowerCase().toCharArray();
+		char b[] = str2.toLowerCase().toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		boolean result =Arrays.equals(a,b);
+		if(result==true) {
+			System.out.println("Strings are anagram");
+		}
+		else {
+			System.out.println("strings are not anagram");
+		}
+	}
