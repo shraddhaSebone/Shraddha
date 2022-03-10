f8ca50bc40ba995863294cc5a56208b315760df8
shraddhaSebone <101252859+shraddhaSebone@users.noreply.github.com>
2022-03-10 23:14:38 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,28 +1,28 @@
 package hello;
 import java.util.*;
 public class Prime {
-
-	public static void main(String[] args) {
-			 Scanner sc=new Scanner(System.in);
-			 System.out.println("enter a number");
-			 int n=sc.nextInt();
-			 int i,j,flag;
-			 for(i=0;i<=n;i++) {
-				 if (i==1||i==0) {
-					 continue;
-				 }
-				 flag=1;
-				 for(j=2;j<=i/2;j++) {
-					 if(i%j==0) {
-						 flag=0;
-						 break;
-					 }
-				 }
-				 if(flag==1) {
-					 System.out.println(i);
-				 }
-
-			 }
-			
+          public void prime(int no) {
+	    	for (int num =2;num <=no;num++) {
+		  boolean isprime =true;
+		    for(int i=2;i<=num/2;i++) {
+			if(num%i==0) {
+		         isprime =false;
+			 break;
+     }
+}
+			if(isprime==true) {
+					System.out.println(num);
+				}
+				
 			}
+	    }
+	public static void main(String args[]) {
+           
+			Scanner scan = new Scanner(System.in);
+			System.out.println("enter a num");
+			int maxnum = scan.nextInt();
+			System.out.println("list of prime no. are:");
+			primeno p=new primeno();
+			p.prime(maxnum);
+	}
 }
