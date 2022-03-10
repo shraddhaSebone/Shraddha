09fc7b06bb3be8f003d28413afff05bd6c46b88c
shraddhaSebone <101252859+shraddhaSebone@users.noreply.github.com>
2022-03-10 23:18:55 +0530

    Update freq1.java

--- freq1.java
+++ freq1.java
@@ -1,21 +1,24 @@
 package hello;
 import java.util.*;
 public class freq1 {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a String");
-		String str=sc.nextLine();
-		System.out.println("Enter a word ");
-		String word=sc.nextLine();
+       public static void main(String[]args) {
+		  
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter a string");
+		String str = sc.nextLine();
+		System.out.println("enter a word");
+		String word= sc.next();
+		frequency_of_occurance f = new frequency_of_occurance();
+		f.freq(str, word);
+	}
+		public void freq(String str,String word) {
+			int c =0;
+		
+		String[] a= str.split(" ");
 	
-		    String a[] = str.split(" ");
-		    int count = 0;
-		    for (int i = 0; i < a.length; i++)
-		    {
-		    if (word.equals(a[i]))
-		        count++;
-	    }
-		    System.out.println(count);
- 	}
+		for(int i=0;i<a.length;i++)
+		     if(a[i].equals(word))
+		    	 c++;
+		System.out.println(c);
+	}
 }
